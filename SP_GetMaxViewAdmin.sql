28d364421425a3a4b2106aa9a22a69660add25c4
Vishu-scs <vishu.b@sparecare.in>
2025-03-05 17:54:10 +0530

    Update SP_GetMaxViewAdmin.sql

--- SP_GetMaxViewAdmin.sql
+++ SP_GetMaxViewAdmin.sql
@@ -1,7 +1,10 @@
 Alter PROCEDURE GetMAXDataAdmin
+    @brandid int ,
 	@DealerID INT ,					  -- Required: DealerID for dynamic table name
     @r1 DECIMAL(10,2) = NULL,         -- Optional: Minimum Avg3MSale
     @r2 DECIMAL(10,2) = NULL,         -- Optional: Maximum Avg3MSale
+	@l1 DECIMAL(10,2) = NULL,         -- Optional: Minimum Landedcost
+    @l2 DECIMAL(10,2) = NULL,         -- Optional: Maximum Landedcost
     @PartNumber VARCHAR(50) = NULL,   -- Optional: Part number filter
     @LocationID INT = NULL,           -- Optional: Location filter                  
     @MaxValueFlag INT = NULL,         -- Optional: NULL -> No filter, 0 -> Maxvalue = 0, 1 -> Maxvalue > 0
@@ -31,6 +34,8 @@ BEGIN
         @d2 NVARCHAR(100),                    -- Dynamic column name for _CS column
         @Dealer NVARCHAR(128),       -- Dynamic table name variable
 		@StockTable NVARCHAR(128),       -- Dynamic table name variable
+		@SPMFeedback NVARCHAR(128),
+		@AdminFeedback NVARCHAR(128),
         @sql NVARCHAR(MAX);                   -- Dynamic SQL statement
 
     -----------------------------------------------------
@@ -38,6 +43,8 @@ BEGIN
     -----------------------------------------------------
     SET @Dealer = '[z_scope].dbo.Dealer_Sale_Upload_Old_TD001_' + CAST(@DealerID AS NVARCHAR(10))+ '(nolock)';
 	SET @StockTable = '[z_scope].dbo.stockable_nonstockable_td001_' + CAST(@DealerID AS NVARCHAR(10))+'(nolock)';
+	SET @SPMFeedback = '[UAD_VON]..UAD_VON_SPMFeedback_'+CAST(@Brandid as NVARCHAR(10));
+	SET @AdminFeedback = '[UAD_VON]..UAD_VON_AdminFeedback_'+CAST(@Brandid as NVARCHAR(10));
 
     -----------------------------------------------------
     -- Generate Dynamic Column List for the past 6 months
@@ -68,7 +75,7 @@ BEGIN
     -- Build the Dynamic SQL Statement
     ------------------------------------------
     SET @sql = N'
-SELECT  
+SELECT 
     li.Brand, 
     li.Dealer, 
     li.location, 
@@ -77,37 +84,58 @@ SELECT
 	 (CASE WHEN li.BrandID = sm.BrandID AND pm.PartNumber = sm.PartNumber THEN sm.SubPartNumber ELSE pm.PartNumber END) AS LatestPartNumber,
     pm.partdesc,  
     pm.category, 
-    pm.mrp, 
+    pm.landedcost, 
     pm.moq, 
 	sn.N1 , sn.N2 , sn.N3,
     sn.Maxvalue, 
     sn.Avg3MSale,
-    ' + @Columnsold + '
+		rm.Remark as SPMRemark , fb.ProposedQty, rm2.Remark as AdminRemark , afb.ApprovedQty,
+    ' + @Columnsold + ',
+	fb.feedbackid
 FROM ' + @Dealer + ' ds
 JOIN ' + @StockTable +' sn 
     ON ds.locationid = sn.locationid AND sn.partnumber = ds.Partnumber
 JOIN z_scope..Part_Master pm 
     ON sn.brandid = pm.brandid AND pm.partnumber = sn.Partnumber 
 JOIN z_scope..locationinfo li on li.locationid = sn.Locationid
-LEFT JOIN norms..Userfeedback ufb on pm.partid = ufb.partid 
 LEFT JOIN PartMgmt_PartNatureMapping pnm
-    ON pm.PartID = pnm.PartID
+    ON ds.PartID = pnm.PartID
 LEFT JOIN partsmodelmapping psm
-    ON pm.PartID = psm.PartID
+    ON ds.PartID = psm.PartID
 LEFT JOIN partmgmt_seasonalmapping psam
-    ON pm.PartID = psam.PartID
+    ON ds.PartID = psam.PartID
+OUTER APPLY (
+    SELECT TOP 1 * 
+    FROM '+ @SPMFeedback +' Fb 
+    WHERE fb.dealerid = li.DealerID 
+      AND fb.locationid = ds.locationid 
+      AND fb.PartID = ds.PartID
+    ORDER BY fb.feedbackid DESC
+) fb
+LEFT JOIN UAD_VON..UAD_VON_RemarksMaster rm on rm.remarkid = fb.UserFBRemarkID
+OUTER APPLY (
+    SELECT TOP 1 * 
+    FROM '+ @AdminFeedback +' aFb 
+    WHERE fb.dealerid = li.DealerID 
+      AND fb.locationid = ds.locationid 
+      AND fb.PartID = ds.PartID
+    ORDER BY afb.AdminFBid DESC
+) afb
+LEFT JOIN UAD_VON..UAD_VON_RemarksMaster rm2 on rm2.remarkid = afb.AdminRemark
 LEFT JOIN substitution_master sm ON sm.BrandID = li.BrandID AND pm.PartNumber = sm.PartNumber
 WHERE sn.stockdate = DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)
    AND(
 		 @Status IS NULL  
-        OR (@Status = 0 AND (ufb.UserFBStatus = ''Pending'' OR ufb.UserFBStatus IS NULL)) 
-         OR (@Status = 1 AND ufb.UserFBStatus = ''Approved'')
+        OR (@Status = 0 AND (fb.Status = ''Pending'' OR fb.Status IS NULL)) 
+         OR (@Status = 1 AND fb.Status = ''Approved'')
    )
     AND (
          @MaxValueFlag IS NULL  
          OR (@MaxValueFlag = 0 AND sn.Maxvalue = 0)
          OR (@MaxValueFlag = 1 AND sn.Maxvalue > 0)
     )
+	AND (@l1 IS NULL OR pm.landedcost > @l1)
+    AND (@l2 IS NULL OR pm.landedcost < @l2)
     AND (@r1 IS NULL OR sn.Avg3MSale > @r1)
     AND (@r2 IS NULL OR sn.Avg3MSale < @r2)
     AND (@PartNumber IS NULL OR sn.partnumber = @PartNumber)
@@ -125,10 +153,12 @@ WHERE sn.stockdate = DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)
     ------------------------------------------
     EXEC sp_executesql 
          @sql, 
-         N'@r1 DECIMAL(10,2), @r2 DECIMAL(10,2), @PartNumber VARCHAR(50), @LocationID INT, @MaxValueFlag INT,
+         N'@r1 DECIMAL(10,2), @r2 DECIMAL(10,2),@l1 Decimal(10,2),@l2 decimal(10,2), @PartNumber VARCHAR(50), @LocationID INT, @MaxValueFlag INT,
            @seasonalid INT, @natureid INT, @modelid INT , @Status bit',
          @r1 = @r1, 
          @r2 = @r2, 
+		 @l1 = @l1,
+		 @l2 = @l2,
          @PartNumber = @PartNumber, 
          @LocationID = @LocationID, 
          @MaxValueFlag = @MaxValueFlag,
@@ -138,5 +168,5 @@ WHERE sn.stockdate = DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)
 		 @Status = @Status;
 END;
 GO
---EXEC GetMAXData @DealerID = 8 , @r1 = null ,@r2 = null,@PartNumber = null,@LocationID = 14,@MaxValueFlag = null,@seasonalid = null,@natureid = null,@modelid = null,@Status = null;
---EXEC GetMAXDataAdmin   20230,null,null,null,40857,null,null,null,null,null;
+--EXEC GetMAXDataAdmin @brandid = 9,@DealerID = 8 , @r1 = null ,@r2 = null,@l1 = null ,@l2 = null,@PartNumber = null,@LocationID = 14,@MaxValueFlag = null,@seasonalid = null,@natureid = null,@modelid = null,@Status = null;
+--use [z_scope] EXEC GetMAXDataAdmin 9,8,null,null,null,null,null,14,null,null,null,null,null;
